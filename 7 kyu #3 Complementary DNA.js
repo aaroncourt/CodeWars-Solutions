eed277c93169a7c9f5ed9526d05628390d5c44d4
Aaron Court <94998913+aaroncourt@users.noreply.github.com>
2023-06-22 13:24:12 -0700

    Create 7 kyu #3 Complementary DNA.js

---
+++ 7 kyu #3 Complementary DNA.js
@@ -0,0 +1,43 @@
+/*
+7 kyu Complementary DNA
+
+Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+Example: (input --> output)
+
+"ATTGC" --> "TAACG"
+"GTAT" --> "CATA"
+*/
+
+
+function DNAStrand(dna){
+  // variable to hold output
+  var outputDNA = '';
+  // read letter at each index of string
+  for (let half of dna){
+    // determine what case it is and then add match to end of output
+    switch (half) {
+      case 'A':
+        outputDNA += 'T';
+        break;
+      case 'T':
+        outputDNA += 'A';
+        break;
+      case 'C':
+        outputDNA += 'G';
+        break;
+      case 'G':
+        outputDNA += 'C';
+        break;
+      default:
+        return 'error'
+    };
+  };
+  return outputDNA
+}
